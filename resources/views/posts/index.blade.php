e9d97cda2ddf2a1fd3d2400ad34daef447a8be50
hamachiii <hamasachi1996@gmail.com>
2025-04-27 02:59:11 +0900

    更新

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -19,17 +19,22 @@
 <body>
    <table>
        <tr>
-          <th>本文</th>
-           <th>タイトル</th>
-           
+            <th>ID</th>
+            <th>タイトル</th>
+            <th>本文</th>
+            <th>作成日時</th>
+            <th>更新日時</th>
           
        </tr>
        
        @foreach($posts as $post)
           <tr>
-          <td>{{ $post->content}}</td>
+          <td>
+          <td>{{ $post->id }}</td>
             <td>{{ $post->title }}</td>
-            
+            <td>{{ $post->content }}</td>
+            <td>{{ $post->created_at }}</td>
+            <td>{{ $post->updated_at }}</td>
           </tr>
                
                
