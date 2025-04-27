0ada27034a9204ce9b20983851942217126dc620
hamachiii <hamasachi1996@gmail.com>
2025-04-27 16:36:09 +0900

    課題提出

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -21,5 +21,29 @@ class PostController extends Controller
 
         return view('posts.show', compact('post'));
     }    
+
+    public function create() {
+        return view('posts.create');
+    }
+
+    public function store(Request $request) {
+        // バリデーションを設定する
+        $request->validate([
+            'title' => 'required|max:20',
+            'content' => 'required|max:200'
+        ]);
+
+        // postsテーブルにデータを追加する
+
+        $post = new Post();
+        $post->title = $request->input('title');
+        $post->content = $request->input('content');
+        $post->save();
+
+        // /postにリダイレクトさせる
+        return redirect('/posts/{post->id}');
+
+
+    }
 }
 
