0f8c16b9dab6af3f67d69d14e937d1310ca9cc34
hamachiii <hamasachi1996@gmail.com>
2025-04-27 17:19:41 +0900

    課題提出

---
+++ posts/index.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Laravel基礎</title>
+</head>
+<body>
+  <h1>投稿一覧</h1>
+  <!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Laravel基礎</title>
+</head>
+
+<body>
+   <table>
+       <tr>
+           
+            <th>タイトル</th>
+            <th>本文</th>
+            
+          
+       </tr>
+       
+       @foreach($posts as $post)
+          <tr>
+          <td>
+        
+            <td>{{ $post->title }}</td>
+            <td>{{ $post->content }}</td>
+            
+          </tr>
+               
+               
+               
+                 
+       @endforeach       
+   </table>
+</body>
+
+</html>
+</body>
+</html>
